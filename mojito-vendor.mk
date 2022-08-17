02194f6a6d3bfb339201d9ccab742117fa053860
Alexander Koskovich <akoskovich@pm.me>
2022-08-17 13:19:25 +0000

    mojito: Drop updatable GPU drivers   * Xiaomi never wrote their own app so QCOM's is just     a stub and this does nothing.
    Change-Id: I740a05b6c1de919786fc9bea141f065f9e3d8c42
    Signed-off-by: Waxaranai <waxaranai@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- mojito-vendor.mk
+++ mojito-vendor.mk
@@ -907,7 +907,6 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.sm6150.api30 \
     PowerOffAlarm \
     ImsRcsService \
     QtiTelephonyService \
